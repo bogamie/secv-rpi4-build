3ac6870b65b0c3f823fe9344060bd42f7a5d6bee
bogamie <bogamie02@gmail.com>
2026-01-10 18:42:50 +0900

    Add sample configuration files for Yocto Project and setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source poky/oe-init-build-env build
+
+echo "Configuring bblayers.conf and local.conf..."
+cp ../configurations/rpi4/local.conf.sample conf/local.conf
+cp ../configurations/rpi4/bblayers.conf.sample conf/bblayers.conf
+
+sed -i "s|##OEROOT##|$(dirname $(pwd))|g" conf/bblayers.conf
+
+echo "=================================================="
+echo " 설정 완료. 이제 아래 명령어로 빌드를 시작하세요:"
+echo " bitbake core-image-base"
+echo "=================================================="
